af4e9245549a508983c5a66c7e7623527e930a3f
Markus Alexander Kuppe <github.com@lemmster.de>
2024-02-08 16:24:17 -0800

    Remove the packet-*.c and packet-*.h dimension.

--- epan/dissectors/dissectors2rfc.py
+++ epan/dissectors/dissectors2rfc.py
@@ -23,13 +23,16 @@ with gzip.open('ietf.json.gz', 'rb') as gzipped_file:
 #     ietf = json.load(json_file)
 
 for filename in os.listdir('.'):
-    if filename.startswith('packet-') and (filename.endswith('.h') or filename.endswith('.c')):
-        with open(filename, 'r') as file:
-            lines = file.readlines()
+    if filename.startswith('') and filename.endswith('.h'):
+        with open(filename, 'r') as header:
+            lines = header.readlines()
+        if os.path.exists(filename.replace(".h", ".c")):
+            with open(filename.replace(".h", ".c"), 'r') as impl:
+                lines += impl.readlines()
         rfcs = set()
         for line in lines:
-            matches = re.search(r'([Rr][Ff][Cc]) ([0-9]+)', line)
+            matches = re.search(r'([Rr][Ff][Cc])\s?([0-9]+)', line)
             if matches:
                 rfcs.add(matches.group(1).upper() + matches.group(2).zfill(4))
         for rfc in rfcs:
-            print(f'{rfc},{pageCount(ietf, rfc)},{filename},{len(lines)},https://tools.ietf.org/html/{rfc},https://github.com/lemmy/wireshark/blob/master/epan/dissectors/{filename}')
+            print(f'{rfc},{pageCount(ietf, rfc)},{filename.replace(".h", "")},{len(lines)},https://tools.ietf.org/html/{rfc},https://github.com/lemmy/wireshark/blob/master/epan/dissectors/{filename},https://github.com/lemmy/wireshark/blob/master/epan/dissectors/{filename.replace(".h", ".c")}')
